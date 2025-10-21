10128628a79207ccbde9cfe2123f22250437837a
Nusaiba Tasnim <139990626+ntpprome@users.noreply.github.com>
2025-10-21 16:42:51 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+// Smooth scroll animation
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+// Greeting message in console
+console.log("Welcome to Nusaiba Tasnim’s Portfolio ✨");
